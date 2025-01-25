819ae8c19441802bddad4552af10493a66503659
mutiara126 <salsamutiara1812@gmail.com>
2025-01-26 03:28:56 +0700

    tambah scaffold dengan TopAppBar untuk navigasi kembali pada halaman detail jenis terapis

---
+++ app/src/main/java/com/example/uas/ui/view/JenisTerapis/DetailJenisTerapis.kt
@@ -0,0 +1,33 @@
+package com.example.uas.ui.view.JenisTerapis
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas.ui.customwidget.CostumeTopAppBar
+import com.example.uas.ui.viewmodel.JenisTerapis.DetailJenisTerapisUiState
+import com.example.uas.ui.viewmodel.JenisTerapis.DetailJenisTerapisViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailJenisTerapisView(
+    navigateBack: () -> Unit,
+    viewModel: DetailJenisTerapisViewModel = viewModel()
+) {
+    val uiState by viewModel.detailJenisTerapisUiState.collectAsState()
+
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = "Detail Jenis Terapis",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }
+    
