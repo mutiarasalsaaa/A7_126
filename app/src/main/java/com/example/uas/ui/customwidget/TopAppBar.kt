dffa257817cdb01e662e0df0826f4e638f6c65a9
mutiara126 <salsamutiara1812@gmail.com>
2025-01-26 03:17:16 +0700

    meningkatkan konsistensi dan fleksibilitas dalam membuat top app bar diseluruh aplikasi

---
+++ app/src/main/java/com/example/uas/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,30 @@
+package com.example.uas.ui.customwidget
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack : Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior ?= null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {},
+)
